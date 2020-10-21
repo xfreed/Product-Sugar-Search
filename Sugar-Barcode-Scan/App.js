062d6e4b1cb2c30d9ae537d86b05fed6abb48c1f
Michael Kunynets <xfreed1@gmail.com>
2020-10-21 15:58:05 +0100

    Init

---
+++ Sugar-Barcode-Scan/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import FindBarcode from "./components/Seaching/SeachingMachine"
+export default function App() {
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    console.log(data);
+    setScanned(true);
+    FindBarcode(data);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  const styles = StyleSheet.create({
+    button: {
+      position: 'absolute',
+      bottom: 0,
+    }
+  })
+
+  return (
+
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {!scanned ?
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+        :
+        <Button onPress={() => setScanned(!scanned)}
+          title="Start"
+          color="#841584"
+          accessibilityLabel="Test" />
+
+      }
+
+
+    </View>
+
+  );
+
+}
